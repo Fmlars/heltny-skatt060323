b6672bd110927c745f739b6e82511b519cc88279
Fmlars <68742126+Fmlars@users.noreply.github.com>
2023-03-06 22:14:22 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
     radio.sendString("Hei")
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(GRPNR)
+})
 input.onButtonPressed(Button.AB, function () {
     radio.setGroup(randint(1, 250))
 })
+let GRPNR = 0
 let TELLER = 0
-radio.setGroup(100)
+GRPNR = 100
+radio.setGroup(GRPNR)
 radio.setTransmitPower(6)
 basic.forever(function () {
     TELLER += 1
